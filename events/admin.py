9b0bd2c45c581e3ec9b74ef3380e9dbbe30a3ada
Sourav Sehgal <60173032+SouravSehgal-3009@users.noreply.github.com>
2021-07-14 12:02:49 +0530

    Add files via upload

---
+++ events/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from mapbox_location_field.admin import MapAdmin
+
+from .models import (
+    EventCategory,
+    Event,
+    EventMember,
+    EventUserWishList,
+    UserMarks
+)
+
+admin.site.register(EventCategory)
+admin.site.register(Event, MapAdmin)
+admin.site.register(EventMember)
+admin.site.register(EventUserWishList)
+admin.site.register(UserMarks)
